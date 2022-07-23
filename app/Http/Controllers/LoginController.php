3145c4f989cf97f85a02e9489953e5de9afdcce2
Artemizzzz <108915524+Artemizzzz@users.noreply.github.com>
2022-07-23 16:14:43 +0700

    Add files via upload

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('Login.Index');
+    }
+
+
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'first_name' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+
+        return back()->with('loginError','Login failed!');
+
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        request()->session()->invalidate();
+
+        request()->session()->regenerate();
+
+        return redirect('/');
+    }
+    //
+}
